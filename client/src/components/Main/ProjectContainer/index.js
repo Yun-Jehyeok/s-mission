95889cb1c6f113a5553b4c894c4f73aacea422f6
YunJehyeok <dbswpgur2@naver.com>
2021-10-01 13:48:16 +0900

    10/1 Project Container

--- client/src/components/Main/ProjectContainer/index.js
+++ client/src/components/Main/ProjectContainer/index.js
@@ -1,171 +1,81 @@
 import React from 'react';
 
-// antd
-import { Card } from 'antd';
-
 // style
-import { OfficeContainer, CardRow } from './style';
+import { OfficeContainer, CardRow, CardContent } from './style';
 
 function ProjectContainer() {
   return (
     <OfficeContainer>
       <CardRow>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        </CardContent>
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            marginBottom: '32px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        </CardContent>
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
+        </CardContent>
       </CardRow>
 
       <CardRow>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        </CardContent>
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            marginBottom: '32px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        </CardContent>
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
+        </CardContent>
       </CardRow>
 
       <CardRow>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        </CardContent>
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            marginBottom: '32px',
-            borderBottom: '2px solid #1990FF',
-          }}
-        >
+        </CardContent>
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
+        </CardContent>
       </CardRow>
 
       <CardRow>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        </CardContent>
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
-        <Card
-          title="Card title"
-          style={{
-            width: 300,
-            height: '213px',
-            borderBottom: '2px solid #1990ff',
-          }}
-        >
+        </CardContent>
+        <CardContent title="Card title">
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
-        </Card>
+        </CardContent>
       </CardRow>
     </OfficeContainer>
   );
